4faa72132963c847d560f5c1526f7e3717e7ac56
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-06-15 22:39:45 +0700

    commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+		args: ['--no-sandbox'],
+	}
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+});
+
+client.initialize();
