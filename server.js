7540b1b842be2e4705bde28031e6253c5e2af38c
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-06-22 14:06:55 +0700

    check

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const qrcode = require('qrcode-terminal');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
@@ -23,3 +29,40 @@ client.on('message', message => {
 });
 
 client.initialize();
+
+app.post('/send', (req, res) => {
+	try {
+		const state = client.getState();
+		const statePromise = new Promise((resolve, reject) => {
+			resolve(state);
+		});
+		statePromise.then( async (value) => {
+			if(value === 'CONNECTED'){
+				let target = '6287861604530@c.us';
+				let data = {
+					name: req.body.name,
+					whatsapp: req.body.whatsapp
+				}
+				let message = `*Data terbaru dari Website LP3I!*\nKami dengan senang hati menginformasikan bahwa data terbaru telah tersedia di website kami:\n\n*Nama lengkap:* ${data.name}\n*Whatsapp:* ${data.whatsapp}\n\nMohon maaf jika pesan ini terkesan otomatis, namun kami ingin memastikan informasi ini tersampaikan dengan tepat dan cepat kepada Anda.\n\nTerima kasih.`
+				client.sendMessage(target, message);
+				return res.json({
+					status: true
+				})
+			}else{
+				console.log(false)
+				return res.json({
+					status: false
+				})
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+		})
+	} catch (error) {
+		console.log(error);
+	}
+})
+
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`);
+})
