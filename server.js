6643414dd2af4ea52f3bd2af13e7c9d7dfc7004c
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-06-23 08:47:39 +0700

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const qrcode = require('qrcode-terminal');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3031;
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
