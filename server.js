075bbf3418e6ba545df9627c72ee4bb6236ebb7e
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-07-18 15:00:59 +0700

    add feedback

--- server.js
+++ server.js
@@ -4,24 +4,26 @@ const cors = require('cors');
 const app = express();
 const port = 4002;
 
+const { phoneNumberFormatter } = require('./helpers/formatter');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
+	authStrategy: new LocalAuth(),
+	puppeteer: {
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
 	}
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+	qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+	console.log('Client is ready!');
 });
 
 client.on('disconnected', (reason) => {
@@ -30,7 +32,7 @@ client.on('disconnected', (reason) => {
 
 client.initialize();
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 	return res.send('Whatsapp BOT Ready 🇮🇩');
 })
 
@@ -40,30 +42,40 @@ app.post('/send', (req, res) => {
 		const statePromise = new Promise((resolve, reject) => {
 			resolve(state);
 		});
-		statePromise.then( async (value) => {
-			if(value === 'CONNECTED'){
+		statePromise.then(async (value) => {
+			if (value === 'CONNECTED') {
 				let target = req.body.target;
 				let data = {
 					name: req.body.name,
+					school: req.body.school,
 					whatsapp: req.body.whatsapp
 				}
-				let message = `*Data terbaru dari Website LP3I!*\nKami dengan senang hati menginformasikan bahwa data terbaru telah tersedia di website kami:\n\n*Nama lengkap:* ${data.name}\n*Whatsapp:* ${data.whatsapp}\n\nMohon maaf jika pesan ini terkesan otomatis, namun kami ingin memastikan informasi ini tersampaikan dengan tepat dan cepat kepada Anda.\nTerima kasih.`
+				let message = `*Data terbaru dari Website LP3I!*\nKami dengan senang hati menginformasikan bahwa data terbaru telah tersedia di website kami:\n\n*Nama lengkap:* ${data.name}\n*Asal sekolah:* ${data.school}\n*Whatsapp:* ${data.whatsapp}\n\nMohon maaf jika pesan ini terkesan otomatis, namun kami ingin memastikan informasi ini tersampaikan dengan tepat dan cepat kepada Anda.\nTerima kasih.`;
 				client.sendMessage(target, message);
+				let feedback = `Halo ${data.name}!\nTerima kasih telah mengisi data, kami senang bisa berkomunikasi dengan Anda. Kami adalah Panitia PMB Politeknik LP3I Kampus Tasikmalaya. Ada yang bisa kami bantu?`;
+				let phone = phoneNumberFormatter(data.whatsapp);
+				client.sendMessage(phone, feedback);
 				return res.json({
 					status: true
 				})
-			}else{
+			} else {
 				console.log(false)
 				return res.json({
 					status: false
 				})
 			}
 		})
-		.catch((error) => {
-			console.log(error);
-		})
+			.catch((error) => {
+				console.log(error);
+				return res.json({
+					status: false
+				})
+			})
 	} catch (error) {
 		console.log(error);
+		return res.json({
+			status: false
+		})
 	}
 });
 
